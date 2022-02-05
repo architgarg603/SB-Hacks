938051bebf75101f69392d281f0cf7402a878f74
Architgarg603 <architgarg603@gmail.com>
2022-02-05 17:24:41 +0530

    landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Landing.module.scss'
+import icon from '../../Assets/icon.png'
 function Landing() {
   return <div className={[style.wrapper, 'data'].join(" ")}>
-      <div className={style.h1}></div>
-      <div className={style.h2}></div>
-      <div className={style.h3}></div>
-      <div className={style.h4}></div>
+    <div className={style.h1}></div>
+    <div className={style.h2}></div>
+    <div className={style.h3}></div>
+    <div className={style.h4}></div>
   </div>;
 }
 
