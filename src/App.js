ab3f6f89748cf99f5e0c973c55b975ac2f5920c7
Architgarg603 <architgarg603@gmail.com>
2022-02-05 17:24:56 +0530

    fix add media

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 import Landing from './Components/Landing/Landing';
+import Homepage from './Components/Homepage/Homepage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<Homepage />} />
           <Route path='/' element={<Landing />} />
         </Routes>
       </BrowserRouter>
